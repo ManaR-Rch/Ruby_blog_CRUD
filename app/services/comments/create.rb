5613570d01a44cdf9eda1d4b16c1562171a1b65d
ManaR-Rch <manarmarchou6@gmail.com>
2025-12-27 17:50:26 +0100

    Add Comments::Create service with spam filtering and transaction

---
+++ app/services/comments/create.rb
@@ -0,0 +1,37 @@
+module Comments
+  class Create < ApplicationService
+    SPAM_KEYWORDS = [
+      "viagra", "casino", "free money", "visit", "link", "http", "https", "porn", "xxx"
+    ].freeze
+
+    def initialize(user:, post:, params:)
+      @user = user
+      @post = post
+      @params = params
+    end
+
+    def call
+      body = @params[:body].to_s.strip
+      return Result.failure(error: :invalid) if body.blank?
+      return Result.failure(error: :spam_blocked) if spam?(body)
+
+      comment = nil
+      ActiveRecord::Base.transaction do
+        comment = @post.comments.build(body: body, user: @user)
+        comment.save!
+        # counter_cache increments automatically due to the association option
+      end
+
+      Result.success(post: comment)
+    rescue ActiveRecord::RecordInvalid
+      Result.failure(error: :invalid)
+    end
+
+    private
+
+    def spam?(text)
+      downcased = text.downcase
+      SPAM_KEYWORDS.any? { |kw| downcased.include?(kw) }
+    end
+  end
+end
