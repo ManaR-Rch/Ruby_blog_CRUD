552357e3f7b12c8c820ffd6220f3d295fa184721
ManaR-Rch <manarmarchou6@gmail.com>
2025-12-30 00:16:47 +0100

    Add authentication redirect and intended page tracking to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :store_user_location!, if: :storable_location?
+  helper_method :current_user
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
 end
